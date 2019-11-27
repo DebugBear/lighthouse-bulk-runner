d87bbd5a249ecc54ec03556c7be4efc0e1f0aa6a
Matt Zeunert <matt@mostlystatic.com>
2019-11-27 10:17:12 +0000

    Output stats

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ allows you to run performance analyses and comparisons
 # example
 
 node lighthouse-bulk-runner.js --urls=testing/urls.txt --configs=testing/configs.json --outDir=out
+
+---
+
+in future might be nice to be able to use the psi api, have some way to split the workload, ...
--- lighthouse-bulk-runner.js
+++ lighthouse-bulk-runner.js
@@ -2,6 +2,7 @@ const program = require('commander');
 const fs = require("fs")
 const collectRunData = require("./collectRunData")
 const hashString = require("./hashString")
+const stats = require("./stats")
 
 program
   .option('--urls <urls>', 'Text file containing a list of URLs you wante to compare (separated by line breaks)')
@@ -13,11 +14,14 @@ program.parse(process.argv);
 const urls = fs.readFileSync(program.urls, "utf-8").split("\n").filter(url => !!url)
 const configs = JSON.parse(fs.readFileSync(program.configs, "utf-8"))
 
+
+
 fs.mkdirSync(program.outDir, { recursive: true })
 fs.mkdirSync(program.outDir + "/lhr", { recursive: true })
 
+
 const runList = []
-const runCount = 1
+const runCount = 3
 
 for (var runIndex = 0; runIndex < runCount; runIndex++) {
   for (const config of configs) {
@@ -34,8 +38,7 @@ for (var runIndex = 0; runIndex < runCount; runIndex++) {
 (async function () {
   for (const [i, run] of Object.entries(runList)) {
     console.log(`Running ${i}/${runList.length} (${run.url.slice(0, 50)})`)
-    let runKey = getRunKey(run)
-    let lhrFilePath = program.outDir + "/lhr/" + runKey + ".json"
+    let lhrFilePath = getLhrPath(run)
 
     if (fs.existsSync(lhrFilePath)) {
       continue
@@ -44,9 +47,77 @@ for (var runIndex = 0; runIndex < runCount; runIndex++) {
     const { lhr } = await collectRunData(run)
     fs.writeFileSync(lhrFilePath, JSON.stringify(lhr, null, 2))
   }
+
+  generateStats()
 })()
+
+function getLhrPath(run) {
+  let runKey = getRunKey(run)
+  return program.outDir + "/lhr/" + runKey + ".json"
+}
+
+function generateStats() {
+  let metrics = [
+    {
+      name: "Performance Score",
+      getValue: lhr => lhr.categories.performance.score * 100,
+      stats: ["median"]
+    },
+    {
+      name: "Page weight (kb)",
+      getValue: lhr => Math.round(lhr.audits["total-byte-weight"].numericValue / 1024 * 10) / 10,
+      stats: ["median"]
+    }, {
+      name: "TTI",
+      getValue: lhr => lhr.audits["interactive"].numericValue,
+      stats: ["median"]
+    }
+  ]
+
+  const headerItems = ["Site/Config"]
+  for (const metric of metrics) {
+    for (const stat of metric.stats) {
+      headerItems.push(`${metric.name} [${stat}]`)
+    }
+  }
+
+  let csv = headerItems.join(",") + "\n"
+
+  for (const url of urls) {
+    for (const config of configs) {
+      let csvLineItems = []
+      let name = url + ` [Config ${getConfigIndexFromHash(configs, getConfigHash(config))}]`
+      csvLineItems.push(name)
+
+      let runResults = []
+      for (let runIndex = 0; runIndex < runCount; runIndex++) {
+        runResults.push(JSON.parse(fs.readFileSync(getLhrPath({ url, config, runIndex }), "utf-8")))
+      }
+
+      for (const metric of metrics) {
+        let metricValues = runResults.map(lhr => metric.getValue(lhr))
+        for (const stat of metric.stats) {
+          csvLineItems.push(stats[stat](metricValues))
+        }
+      }
+
+      csv += csvLineItems.join(",") + "\n"
+    }
+  }
+
+  console.log(csv)
+  fs.writeFileSync(program.outDir + "/stats.csv", csv)
+}
 
 function getRunKey(run) {
   let urlKey = run.url.replace(/https?\:\/\//, "").replace(/\//g, "-").replace(/[^a-zA-Z0-9\-\.]/g, "")
-  return urlKey + "_" + hashString(JSON.stringify(run.config)) + "_" + run.runIndex
+  return urlKey + "_" + getConfigHash(run.config) + "_" + run.runIndex
+}
+
+function getConfigHash(config) {
+  return hashString(JSON.stringify(config))
+}
+
+function getConfigIndexFromHash(configs, configHash) {
+  return configs.map(config => getConfigHash(config)).indexOf(configHash)
 }
