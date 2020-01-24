b619d8c16b98ea0d334b42dba5c6db5d25f61d23
Josh <northwestbased@gmail.com>
2020-01-24 00:02:14 -0800

    add --headless flag back to chrome config

--- lighthouseServer/collectRunData.js
+++ lighthouseServer/collectRunData.js
@@ -2,7 +2,7 @@ const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
 
 module.exports = async function ({ url, config }) {
-  return chromeLauncher.launch({chromeFlags: ['--no-sandbox']}).then(chrome => {
+  return chromeLauncher.launch({chromeFlags: ['--no-sandbox', '--headless']}).then(chrome => {
     return lighthouse(url, { port: chrome.port }, config).then(results => {
       return chrome.kill().then(() => results)
     });
