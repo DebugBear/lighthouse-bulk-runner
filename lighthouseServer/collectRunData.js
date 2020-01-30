fcb1fadcce87a56b827d7a8bfd8d63b0838e047c
Josh <northwestbased@gmail.com>
2020-01-30 15:27:26 -0800

    cleans up and fixes errors

--- lighthouseServer/collectRunData.js
+++ lighthouseServer/collectRunData.js
@@ -2,9 +2,9 @@ const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
 
 module.exports = async function ({ url, config }) {
-  return chromeLauncher.launch({chromeFlags: ['--no-sandbox', '--headless']}).then(chrome => {
-    return lighthouse(url, { port: chrome.port }, config).then(results => {
-      return chrome.kill().then(() => results)
-    });
+  return chromeLauncher.launch({ chromeFlags: ['--no-sandbox', '--headless'] }).then(chrome => {
+    return lighthouse(url, { port: chrome.port }, config)
+      .catch(error => ({ error }))
+      .finally(() => chrome.kill())
   })
 }
--- lighthouseServer/server.js
+++ lighthouseServer/server.js
@@ -4,8 +4,6 @@ const Compute = require("@google-cloud/compute");
 
 const ZONE_NAME = 'us-central1-a'
 
-const LH_ATTEMPTS = 4
-
 async function getQueueServerUrl() {
   return await request({
     uri: 'http://metadata.google.internal/computeMetadata/v1/instance/description',
@@ -15,13 +13,6 @@ async function getQueueServerUrl() {
     response.queueServerUrl
   )).catch((e) => {
     console.log(e)
-  })
-}
-
-async function getNextUrl(url) {
-  return await request(url).then((res) => {
-    response = JSON.parse(res)
-    return response
   })
 }
 
@@ -37,46 +28,32 @@ async function deleteInstance() {
 (async function () {
   let queueServerUrl = await getQueueServerUrl().catch((e) => {
     console.log(e)
-    deleteInstance()
+    return deleteInstance()
   })
 
-  let moreUrls = true
-  while (moreUrls) {
-    let urlData = await getNextUrl(queueServerUrl + "/getUrl").catch((e) => {
-      moreUrls = false
-      deleteInstance()
+  async function getNextUrl() {
+    return await request(queueServerUrl + "/getUrl").then((res) => (
+      response = JSON.parse(res)
+    )).catch((e) => {
+      console.log(e)
+      return null
     })
+  }
 
-    if (urlData) {
-      console.log(urlData)
-      let error = undefined
+  const postResult = async (data) => {
+    await request.post(queueServerUrl + "/postResult", {
+      json: data
+    }).catch(console.log)
+  }
 
-      let data = await collectRunData(urlData).catch(function (e) {
-        error = e
-        console.log("\n\nlighthouse error here:\n\n", error)
-      })
-      if (data && data.lhr && data.lhr.runtimeError) {
-        error = data.lhr.runtimeError
-        console.log("\n\nruntime error here:\n\n", error)
-      }
-
-      if (typeof error !== 'undefined') {
-        await request.post(queueServerUrl + "/postResult", {
-          json: { error: error, response: urlData }
-        }).catch((e) => {
-          console.log(e)
-        })
-      }
-      else {
-        console.log(`returning success!`)
-        await request.post(queueServerUrl + "/postResult", {
-          json: { result: data, response: urlData }
-        }).catch((e) => {
-          console.log(e)
-        })
-      }
+  let runSettings = null
+  while (runSettings = await getNextUrl()) {
+    let runResult = await collectRunData(runSettings)
+    if (runResult.error)
+      await postResult({ runResult, runSettings })
+    else {
+      await postResult({ runResult: { lhr: runResult.lhr }, runSettings })
     }
   }
+  await deleteInstance()
 })()
-
-
